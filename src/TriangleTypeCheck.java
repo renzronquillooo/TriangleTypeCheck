1cce6cf604cbcdae84eeac38377d6096d1e28b43
jrdronquillo <jrdronquillo@gmail.com>
2025-01-11 05:53:05 +0800

    added sideg

--- src/TriangleTypeCheck.java
+++ src/TriangleTypeCheck.java
@@ -24,6 +24,9 @@ public class TriangleTypeCheck {
 		System.out.print("Please enter the length of third side: ");
 		int sidef = scanner.nextInt();
 		
+		System.out.print("Please enter the length of third side: ");
+		int sideg = scanner.nextInt();
+		
 		//Verify if it is a triangle or not by calling method IsTriangle then if yes (true), verify the type of triangle, else the sides ae not forming a triangle
 		if (isTriangle(sidea, sideb, sidec) == true) {
 			if (sidea == sideb && sideb == sidec && sidea == sidec) {
